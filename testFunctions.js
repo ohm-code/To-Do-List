0b752ed3d420690d1a92617f7efe950fc823e7b0
ohmcode <email@jasperfu.com>
2021-12-13 03:03:04 -0500

    working with local data persistance

--- testFunctions.js
+++ testFunctions.js
@@ -9,6 +9,7 @@ const projectDropDown = document.querySelector('#projectDropDown')
 const addTaskForm = document.getElementById('addTask');
 const addProjectForm = document.getElementById('addProject')
 let projectListObject ={}
+let taskList = document.querySelector('.taskList')
 let taskDetails = document.querySelector('.taskDetails')
 const createTaskButton = document.getElementById('createTask')
 const createProjectButton = document.getElementById('createProject')
@@ -24,10 +25,10 @@ const createTask = (task, project,priority,dueDate,details)=>{ // need to see if
 const addTaskProject = (taskObject,projectObject) =>{
     projectObject.push(taskObject);
 }
-const addMultipleTaskProject = (taskArray, project) => {
+const addMultipleTaskProject = (taskArray, projectObject) => {
+    console.log(taskArray)
     taskArray.forEach(element => {
-        project.push(element)})}
-
+        projectObject.push(element)})}
 
 
 const indexOfTask = (value,key, array) =>{ //returns first index, potential issue if two tasks are named the asame
@@ -40,51 +41,50 @@ const indexOfTask = (value,key, array) =>{ //returns first index, potential issu
     return i
 }
         
-const projectFunctions = (()=>{
-    const create= (projectName) =>{
-        let project = document.createElement('ul');
+ const createProjectObject = (projectKey) =>{
+    projectListObject[projectKey] = [{task:"PLACEHOLDER", project:projectKey, priority:"", dueDate: "", details:"placeholder"}]; //initiate key value for new project
+    console.log(projectKey + " project object created!")
+    console.log("check to make sure PLO has updated after creating project ob")
+    console.log(projectListObject) 
+    // 
+    console.log(!document.getElementById(projectKey))
+    if ((!document.getElementById(projectKey))&&(projectKey!='Unassigned')&&(projectKey!='Completed')){
+        createProjectNode(projectKey)
+    }
+ }
+ const createProjectNode = (projectKey) =>{ //creates project node to add to project list node
+        let project = document.createElement('ul'); //create project node
+            project.className = 'project'; // defining project node 
+            project.id = projectKey; 
+            project.textContent = projectKey;
 
-        let projectOption = document.createElement('option')
-            projectOption.value = projectName;
-            projectOption.textContent = projectName; 
-            projectOption.id = projectName; 
-        
-        projectDropDown.appendChild(projectOption); 
-            project.className = 'project';
-            project.id = projectName; 
-            project.textContent = projectName
-        let button = document.createElement('button');
-            button.addEventListener('click', () => project.remove())
+        let projectOption = document.createElement('option')//creating and adding dropdown options
+            projectOption.value = projectKey;
+            projectOption.textContent = projectKey; 
+            projectOption.id = projectKey; 
+            projectDropDown.appendChild(projectOption); 
+
+        let button = document.createElement('button'); //creating X button for project, should also remove dropdown option
             button.textContent = 'x';
-            project.appendChild(button);        
-        projectList.appendChild(project);
-            projectNodeList = document.querySelectorAll('.project')
-            projectName = [];
-            projectListObject[project.id] = projectName;
-            project.addEventListener('click', ()=>{
-            
-            const taskList = document.querySelector('.taskList')
-            changeNode(taskList,createTaskListNode(projectName))   
-        }) 
-        return projectName
-    }
-    const info = (projectName) =>{
-    }
+            button.addEventListener('click', () => {
+                let option = document.getElementById(projectOption.id)
+                option.remove()
+                project.remove()
+        })
+            project.appendChild(button);    
+            projectList.appendChild(project);
+            projectNodeList = document.querySelectorAll('.project') //update project Node List
 
-    return {
-        create,
-        info
-    }
-}) ()
+            project.addEventListener('click', ()=>{ //shows task list 
+                updateTaskListNode(getProjectObject(projectKey))
+        }) }
 
-
-const projectTaskList = (project) =>{
-    let taskList = [];
-    project.forEach((task)=>{
-        taskList.push(task.task)
-    })
-    return taskList;
+ 
+const updateTaskListNode = (projectObject) =>{
+    taskList = document.querySelector('.taskList')
+    changeNode(taskList,createTaskListNode(projectObject))   
 }
+
 
 const returnProjectNode = (projectName)=>{ 
 let projectNode
@@ -114,10 +114,10 @@ const createTaskNode = (taskObject) => { //input task object, output task node
     const p = document.createElement('p');
     let taskNode = document.createElement('div')
     taskNode.className = 'taskDetails';
+    taskNode.id = 'taskDetailsHeader'
 
     let nameD = document.createElement('h2');
         nameD.textContent = "Task: "
-        nameD.id = "taskHeader"
         
     let projectD = document.createElement('strong');
         projectD.textContent = "Project: ";
@@ -132,9 +132,9 @@ const createTaskNode = (taskObject) => { //input task object, output task node
         detailsD.textContent= "Details: ";
       
 
- 
     let name = document.createElement('h2');
         name.textContent = taskObject.task;
+        name.style.marginLeft = '2rem';
         taskNode.appendChild(nameD);
         taskNode.appendChild(name);
         taskNode.appendChild(p);
@@ -159,13 +159,18 @@ const createTaskNode = (taskObject) => { //input task object, output task node
         taskNode.appendChild(details);
         taskNode.appendChild(p);
 
+    [name,priority, project, due,details].forEach((element)=>{
+            element.className = 'taskDetailsItem'
+        })
+ 
+
     let markCompleteButton = document.createElement('button')
         markCompleteButton.addEventListener('click', ()=>{//**change button color? */
             taskObject.project = 'Completed';
-            addTaskProject(taskObject,getProjectArray('Completed'))
-            removeTask(taskObject,getProjectArray(projectNameRef))
+            addTaskProject(taskObject,getProjectObject('Completed'))
+            removeTask(taskObject,getProjectObject(projectNameRef))
             let taskList = document.querySelector('.taskList')
-            changeNode(taskList, createTaskListNode(getProjectArray(projectNameRef)))
+            changeNode(taskList, createTaskListNode(getProjectObject(projectNameRef)))
         }) 
         taskNode.appendChild(markCompleteButton) 
     
@@ -173,11 +178,12 @@ const createTaskNode = (taskObject) => { //input task object, output task node
 
     let editTaskDetailsButton = document.createElement('button') //**add functionality
         editTaskDetailsButton.addEventListener('click', ()=>{
-            saveButton.style.visibility = 'visible'
-            editTaskDetailsButton.style.visibility = 'hidden';
+            saveButton.style.display = 'inline-block';
+            markCompleteButton.style.display = 'none';
+            editTaskDetailsButton.style.display = 'none';
             name.contentEditable = 'true';
             project.contentEditable = 'true';
-            date.contentEditable = 'true';
+            due.contentEditable = 'true';
             priority.contentEditable = 'true';
             details.contentEditable = 'true';
             [name, project, due, priority, details].forEach((element)=>
@@ -187,16 +193,17 @@ const createTaskNode = (taskObject) => { //input task object, output task node
         taskNode.appendChild(editTaskDetailsButton)
         
     let saveButton = document.createElement('button') 
-        saveButton.style.visibility = 'hidden'; 
+        saveButton.style.display = 'none'; 
         saveButton.textContent = "Save Changes";
         saveButton.addEventListener('click', ()=>{
-            editTaskDetailsButton.style.visibility = 'visible';
-            saveButton.style.visibility = 'hidden';
+            markCompleteButton.style.display = 'inline-block';
+            editTaskDetailsButton.style.display = 'inline-block';
+            saveButton.style.display = 'none';
             [name, project, due, priority, details].forEach((element)=>{
                 element.style.background = 'transparent';
                 element.contentEditable = 'false';
             })
-            let updateTaskDetails = document.querySelector('.taskDetails').childNodes; 
+            let updateTaskDetails = document.querySelectorAll('.taskDetailsItem'); 
             let taskObject = {
                 task: updateTaskDetails[0].textContent,
                 project: updateTaskDetails[1].textContent,
@@ -205,23 +212,26 @@ const createTaskNode = (taskObject) => { //input task object, output task node
                 details: updateTaskDetails[4].textContent
             };
             console.log(taskObject)
-            removeTask(taskNode,getProjectArray(projectNameRef))
-            console.log(getProjectArray(projectNameRef)) 
-            addTaskProject(taskObject,getProjectArray(projectNameRef)) 
-            console.log(getProjectArray(projectNameRef));
+            removeTask(taskNode,getProjectObject(projectNameRef))
+            console.log(getProjectObject(projectNameRef)) 
+            addTaskProject(taskObject,getProjectObject(projectNameRef)) 
+            console.log(getProjectObject(projectNameRef));
             let taskList = document.querySelector('.taskList'); 
             
-            changeNode(taskList,createTaskListNode(getProjectArray(projectNameRef)) )
+            changeNode(taskList,createTaskListNode(getProjectObject(projectNameRef)) )
         })
 
         taskNode.appendChild(saveButton)// change to only display if edit is click
         
     let removeTaskButton = document.createElement('button')//**add functionality */
-        removeTaskButton.addEventListener('click',()=>{
-            removeTask(taskObject,getProjectArray(projectNameRef))
+        removeTaskButton.addEventListener('click',(event)=>{
+            event.preventDefault()
+            console.log(taskObject)
+            console.log(getProjectObject(projectNameRef))
+            removeTask(taskObject,getProjectObject(projectNameRef))
             changeNode(taskNode,defaultTaskNode) //swap with default
             let taskList = document.querySelector('.taskList')
-            changeNode(taskList, createTaskListNode(getProjectArray(projectNameRef)))
+            changeNode(taskList, createTaskListNode(getProjectObject(projectNameRef)))
         })// * remove from project object *do  change node 
         removeTaskButton.textContent='Delete Task'
         taskNode.appendChild(removeTaskButton)
@@ -231,17 +241,18 @@ const createTaskNode = (taskObject) => { //input task object, output task node
 
 const createTaskListNode = (project) => {
     let node = document.createElement('div');
-    
     node.className = 'taskList';
     let header = document.createElement('h2')
     header.textContent = "Tasks: "
     header.style.display = 'inline-block';
     node.appendChild(header)
+    console.log(project)
 
-
-    project.forEach((task) =>{
-        
-         let taskNode = document.createElement('ul')
+    project.forEach((task,index) =>{
+        console.log(index)
+        if(index < 1){console.log('placeholder')
+        }else
+         {let taskNode = document.createElement('ul')
             taskNode.addEventListener('click',()=>{
                 taskDetails = document.querySelector('.taskDetails') 
                 changeNode(taskDetails,createTaskNode(task)) 
@@ -249,17 +260,21 @@ const createTaskListNode = (project) => {
             taskNode.className = task.task;
             taskNode.textContent = task.task;
             let button = document.createElement('button');
-            button.addEventListener('click',()=>{
+            button.addEventListener('click',(event)=>{
+                event.preventDefault();
+                console.log('remove triggered')
+                let projectName = getProjectName[project];
                 taskNode.remove()
-   
                 let index = indexOfTask(task.task,'task',project)
+                console.log(projectName)
                 project.splice(index,1)
-           
+                console.log(projectListObject[projectName])//expected the key reference to project in the list
+                projectListObject[projectName] = project;
             });//remove from project array as well
             button.textContent = "x";
-
+            
             taskNode.appendChild(button)
-            node.appendChild(taskNode)
+            node.appendChild(taskNode)}
         }) 
     return node
     }
@@ -274,31 +289,30 @@ const submitaddTaskForm = (event)=>{
     let task = createTask(f['task'].value,f['projectDropDown'].value, f['priority'].value,f['date'].value, f['details'].value,)
     f['task'].value;
     let project = f['projectDropDown'].value;
+    console.log(project)
     projectListObject[project].push(task)
-    let node = createTaskListNode(getProjectArray(project))
+    let node = createTaskListNode(getProjectObject(project))
     const taskList = document.querySelector('.taskList')
     changeNode(taskList,node)
     addTaskForm.style.display = 'none';
-  
-   // project.appendChild(taskNode)
-   // let projectNode = document.getElementById(project);
 }
 
 const submitAddProjectForm = (event) => {
     event.preventDefault() 
     let projectName = addProjectForm.elements['projectName'].value; 
-    projectFunctions.create(projectName)
+    createProjectObject(projectName)
     addProjectForm.style.display = 'none';
-    
+    addToLocalStorage()
 }
 
 addTaskForm.addEventListener('submit', submitaddTaskForm)
 addProjectForm.addEventListener('submit',submitAddProjectForm)
 
-const getProjectArray = (projectName) =>{
+const getProjectObject = (projectName) =>{
     return projectListObject[projectName] 
 }
 const getProjectName = (projectArray) =>{
+  
     return projectArray[0].project
 }
 
@@ -307,18 +321,20 @@ const removeTask = (taskObject, projectObject) =>{
     projectObject.splice(index,1)
 }
 //do not delete, initialization
-projectFunctions.create('Unassigned'); //create unassigned category, remove typical project button options
-const unassignedProjectNode = document.getElementById('Unassigned')
-const unassignedButton = unassignedProjectNode.getElementsByTagName('button')
-unassignedButton[0].remove()
-projectListContainer.childNodes[1].after(unassignedProjectNode)    
+createProjectObject('Unassigned')
+createProjectNode('Unassigned'); //create unassigned category, remove typical project button options
+    const unassignedProjectNode = document.getElementById('Unassigned')
+    const unassignedButton = unassignedProjectNode.getElementsByTagName('button')
+    unassignedButton[0].remove()
+    projectListContainer.childNodes[1].after(unassignedProjectNode)    
 
 //
-projectFunctions.create('Completed'); //create unassigned category, remove typical project button options
-const completedTasksNode = document.getElementById('Completed')
-const completedButton = completedTasksNode.getElementsByTagName('button')
-completedButton[0].remove()  
-projectList.after(completedTasksNode) 
+createProjectObject('Completed')
+createProjectNode('Completed'); //create unassigned category, remove typical project button options
+    const completedTasksNode = document.getElementById('Completed')
+    const completedButton = completedTasksNode.getElementsByTagName('button')
+    completedButton[0].remove()  
+    projectList.after(completedTasksNode) 
 //
 createTaskButton.addEventListener('click', () => {
     addTaskForm.style.display = 'inline-block';
@@ -328,12 +344,57 @@ createProjectButton.addEventListener('click', ()=> {
     addProjectForm.style.display = 'inline-block';
 }) 
 
-const defaultTask = createTask("","","","","")
-const defaultTaskNode = createTaskNode(defaultTask)
-console.log(defaultTaskNode)
+
 //show addTaskForm on click 
 /// end initialization
 
-projectFunctions.create('project1')
-let test1 = createTask('test1','project1','priority', 'dueDate','details')
-addTaskProject(test1, getProjectArray('project1'))
+
+document.addEventListener('click',()=>addToLocalStorage())
+
+const addToLocalStorage = () =>{
+    localStorage.setItem("projectListObject",JSON.stringify(projectListObject))
+    console.log('add to local storage triggered')
+}
+
+const getFromLocalStorage = () =>{
+    console.log(' get from local storage triggeredworks!')
+    const storage = JSON.parse(localStorage.getItem("projectListObject"))
+    if (storage === null){console.log("nothing in storage, dont render")
+    }else return storage
+}
+
+const renderPage = () =>{
+    const localProjectListObject = getFromLocalStorage()
+    projectListObject = localProjectListObject;
+    console.log("render page local project list object")
+    console.log(localProjectListObject)
+    Object.keys(localProjectListObject).forEach((project)=>{ // goes through each key 
+        console.log(project)
+        console.log(localProjectListObject[project])
+        console.log(localProjectListObject[project].length)
+
+        if ((project!='Unassigned')&&(project!='Completed')){
+            createProjectNode(project)
+            console.log(project + " object node created")
+            return;
+        }
+        //need to catch if key.length > 1 since all array will have at least 1 hidden placeholder
+        if (localProjectListObject[project].length>1){
+             //anything that has non-placeholder tasks
+            localProjectListObject[project].forEach((taskObject, taskNumber)=>{//loops through the actual task objects 
+                console.log(taskNumber) // index of task for this project, need to avoid 0 since it is placeholder
+                console.log(taskObject)//tasks for this project
+                if (taskNumber > 0){
+                    console.log(taskObject) //just non placeholder 
+                     //create project node      
+                }
+            
+            }) 
+        }
+    }
+    )
+
+    }
+if (getFromLocalStorage()!==null){
+    renderPage()
+}
